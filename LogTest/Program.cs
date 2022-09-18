9fc34091b0833f7a08e922db5d2bf43bd9d92f20
joaquinaguerotrevisan <joaquinaguerotrevisan@gmail.com>
2022-09-18 13:34:27 -0300

    LogTest addes

---
+++ LogTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Program
+{
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+    public static void Main()
+    {
+        try
+        {
+            Logger.Info("Info...");
+            System.Console.ReadKey();
+
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex, "Error...");
+            Logger.Fatal(ex, "Fatal...");
+        }
+
+        NLog.LogManager.Shutdown();
+    }
+}  
